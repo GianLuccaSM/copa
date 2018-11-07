4e388506c291e7d184dcc8b189747152cd6be287
unknown <digo10.silva@gmail.com>
2018-11-07 20:57:02 -0200

    v2

---
+++ atributos.py
@@ -0,0 +1,19 @@
+# Atributos
+from random import randint
+import sys
+import time
+
+
+
+atributosTimes_1 = [randint(40, 50) for i in range(0, 8)]
+atributosTimes_2 = [randint(30, 50) for i in range(0, 8)]
+atributosTimes_3 = [randint(20, 50) for i in range(0, 8)]
+atributosTimes_4 = [randint(10, 50) for i in range(0, 8)]
+
+print(atributosTimes_1)
+time.sleep(5)
+print(atributosTimes_2)
+time.sleep(5)
+print(atributosTimes_3)
+time.sleep(5)
+print(atributosTimes_4)
